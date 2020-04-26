2bf8ae3001b764ebeba58bcc2da4f590f894ab6e
Electronim <petrascosandu@gmail.com>
2020-04-26 14:16:20 +0300

    add: temp

--- SplitIt/app/src/main/java/com/example/splitit/ui/friends/FriendsFragment.java
+++ SplitIt/app/src/main/java/com/example/splitit/ui/friends/FriendsFragment.java
@@ -7,16 +7,38 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.splitit.R;
 
+import java.util.ArrayList;
+
 public class FriendsFragment extends Fragment {
+    private static final String TAG = "FriendsFragment";
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_friends, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        textView.setText("Friends fragment!");
-        return root;
+        return inflater.inflate(R.layout.fragment_friends, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mRecyclerView = view.findViewById();
+        mRecyclerView.setHasFixedSize(true);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mAdapter = new FriendsAdapter(new ArrayList<>, this);
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+
 }

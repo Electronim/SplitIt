52cb548e4bc4deb5b52e3f8d7c7029bf29649be9
Electronim <petrascosandu@gmail.com>
2020-04-29 04:15:06 +0300

    add: more ui design incoming

--- SplitIt/app/src/main/java/com/example/splitit/ui/adapter/ActionAdapter.java
+++ SplitIt/app/src/main/java/com/example/splitit/ui/adapter/ActionAdapter.java
@@ -11,7 +11,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.splitit.R;
 import com.example.splitit.model.Action;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class ActionAdapter extends RecyclerView.Adapter<ActionAdapter.ActionViewHolder> {
     private ArrayList<Action> mActions;
@@ -36,6 +39,22 @@ public class ActionAdapter extends RecyclerView.Adapter<ActionAdapter.ActionView
     public void onBindViewHolder(@NonNull ActionViewHolder holder, int position) {
         Action action = mActions.get(position);
         holder.actionMessage.setText(action.message);
+
+        long millis = action.timestamp;
+
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTimeInMillis(millis);
+
+        int hour = calendar.get(Calendar.HOUR);
+        int minute = calendar.get(Calendar.MINUTE);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        String month = new SimpleDateFormat("MMM").format(calendar.getTime());
+
+        String timeStr = String.format("%02d:%02d", hour, minute);
+        String dateStr = month + " " + day;
+
+        holder.timeInfo.setText(timeStr);
+        holder.dateInfo.setText(dateStr);
     }
 
     @Override
@@ -45,10 +64,14 @@ public class ActionAdapter extends RecyclerView.Adapter<ActionAdapter.ActionView
 
     public class ActionViewHolder extends RecyclerView.ViewHolder {
         public TextView actionMessage;
+        public TextView timeInfo;
+        public TextView dateInfo;
 
         public ActionViewHolder(@NonNull View itemView) {
             super(itemView);
             this.actionMessage = itemView.findViewById(R.id.textView_actionMessage);
+            this.timeInfo = itemView.findViewById(R.id.textView_time);
+            this.dateInfo = itemView.findViewById(R.id.textView_date);
         }
     }
 }
